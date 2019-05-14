748674baaf66713fae2ecccd8a6ace7c82fa2090
adoankim <adoankim@gmail.com>
2019-05-14 21:26:04 +0200

    Add get active icon name method for iOS

--- ios/RNDynamicAppIcon.m
+++ ios/RNDynamicAppIcon.m
@@ -16,6 +16,13 @@ RCT_EXPORT_METHOD(setAppIcon:(NSString *)name)
   }];
 }
 
+RCT_REMAP_METHOD(getActiveIconName,
+                 getActiveIconNameWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    resolve([[UIApplication sharedApplication] alternateIconName]);
+}
+
 RCT_REMAP_METHOD(supportsDynamicAppIcon, resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
   bool supported = [[UIApplication sharedApplication] supportsAlternateIcons];
